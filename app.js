cd89c1ede22cda9a073446fad653d96df80bc17e
Artem Klimenko <artemklimenko-dev@gmail.com>
2022-11-06 07:33:19 +0200

    added image processing function

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const Jimp = require('jimp');
 const express = require('express')
 const app = express()
 const port = 3000
@@ -29,7 +31,15 @@ app.post('/rss', (req, resp) => {
             future = res.data
             return parser.parseString(res.data)
         }).then(res => {
-            links[url] = res
+            if (res.items) {
+                res.items.forEach((item, i) => {
+                    if (item.enclosure.type === "image/jpeg") {
+                        let imgpath = process(item.enclosure.url, 1)
+                        res.items[i].enclosure.url = imgpath
+                    }
+                })
+            }
+            links[url] = res 
         })
         .catch(err => console.log(err))
     });
@@ -57,4 +67,67 @@ combineRss = (lnk) => {
         ls.items.push(link.items)
     })
     return ls
+}
+
+function process(url, dither_bits) {
+    let filepath = "public/" + url.split("/").pop();
+
+    if (fs.existsSync(filepath)) {
+       return filepath;
+    }
+
+    Jimp.read(url, (err, image) => {
+        if (err) throw err;
+
+        if (image.bitmap.height > 400) {
+            image = image.resize(Jimp.AUTO, 400);
+        }
+        image = image.greyscale();
+
+        dither_threshold = 2**dither_bits
+
+        for (let hi = 0; hi < image.bitmap.height; ++hi) {
+            let i = hi * image.bitmap.width * 4;
+            for (let wj = 0; wj < image.bitmap.width; ++wj) {
+                let j = wj * 4;
+                let j_next = j + 4;
+                let p = image.bitmap.data[i + j];
+                var p_new = Math.round(p / 255 * (dither_threshold-1));
+                p_new = Math.round(p_new * (255 / (dither_threshold-1)))
+                
+                image.bitmap.data[i + j]     = p_new;
+                image.bitmap.data[i + j + 1] = p_new;
+                image.bitmap.data[i + j + 2] = p_new;
+
+                q_err = p - p_new;
+
+                image.bitmap.data[i + j_next]     += q_err * (7 / 16);
+                image.bitmap.data[i + j_next + 1] += q_err * (7 / 16);
+                image.bitmap.data[i + j_next + 2] += q_err * (7 / 16);
+
+                let i_next = (hi+1)*image.bitmap.width*4;
+                
+                if (hi === 215 && wj === 147) {
+                }
+
+                if (hi > 0) {
+                    let j_prev = j - 4;
+                    image.bitmap.data[i_next + j_prev] += q_err * (3 / 16);
+                    image.bitmap.data[i_next + j_prev + 1] += q_err * (3 / 16);
+                    image.bitmap.data[i_next + j_prev + 2] += q_err * (3 / 16);
+                }
+
+                image.bitmap.data[i_next + j] += q_err * (5 / 16);
+                image.bitmap.data[i_next + j + 1] += q_err * (5 / 16);
+                image.bitmap.data[i_next + j + 2] += q_err * (5 / 16);
+
+                image.bitmap.data[i_next + j_next]     += q_err * (1 / 16);
+                image.bitmap.data[i_next + j_next + 1] += q_err * (1 / 16);
+                image.bitmap.data[i_next + j_next + 2] += q_err * (1 / 16);
+            }
+        }
+        
+        image.write(filepath);
+    });
+    return filepath;
 }
